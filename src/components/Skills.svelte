5338ae98d5df94c9287bd5a0df4c75d2500a43dd
Alan <john1337@gmail.com>
2022-01-29 22:10:40 -0600

    About

---
+++ src/components/Skills.svelte
@@ -0,0 +1,80 @@
+<script>
+	import SkillBars from './SkillBars.svelte';
+
+	const skillsLevels = [
+		{
+			skills: 'HTML',
+			icono: 'html-5.svg',
+			porcentaje: 80,
+			color: 'green',
+			delay: 300
+		},
+		{ skills: 'CSS', icono: 'css-3.svg', porcentaje: 50, color: 'blue', delay: 600 },
+		{
+			skills: 'JavaScript',
+			icono: 'javascript.svg',
+			porcentaje: 40,
+			color: 'yellow',
+			delay: 1000
+		},
+		{
+			skills: 'MySql',
+			icono: 'mysql.svg',
+			porcentaje: 30,
+			color: 'red',
+			delay: 1300
+		},
+		{
+			skills: 'SvelteJs',
+			icono: 'svelte-icon.svg',
+			porcentaje: 40,
+			color: 'orange',
+			delay: 1600
+		}
+	];
+</script>
+
+<main class="container">
+	<section class="Skills">
+		<div class="">
+			<h2 class="Skills-titulo">Habilidades</h2>
+		</div>
+
+		<div class="skills-content">
+			<div class="skills-experiencia">
+				<p>
+					Desarrollador Frontend con experiencia en maquetacion con HTML y CSS y el lengueje de
+					programación JavaScript.
+				</p>
+				<p>Uso del Framework SvelteJs para la elaboracion de aplicaciones SPA y SSR</p>
+				<p>Conocimiento basico de SQL</p>
+			</div>
+			<div class="skills-bar">
+				<div class="bars">
+					<div class="info">
+						{#each skillsLevels as { skills, icono, porcentaje, color, delay }}
+							<SkillBars {skills} {icono} {porcentaje} {color} {delay} />
+						{/each}
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+
+<style>
+	
+	.Skills-titulo {
+		text-align: center;
+		font-size: 3rem;
+		font-weight: bold;
+	}
+
+	@media (min-width: 768px) {
+		.skills-content {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			gap: 5rem;
+		}
+	}
+</style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,11 +1,42 @@
+
+<script>
+	import Skills from '../Components/Skills.svelte';
+</script>
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
 
-<div class="About">
-	<h1>Alan</h1>
-	
-	<p>This is the 'about' page. There's not much here.</p>
-
+<div class="container">
+	<section class="entrada">
+		<Skills />
+	</section>
 </div>
+
+<style>
+	:global(html) {
+		box-sizing: border-box;
+		font-size: 62.5%; /**1rem = 10px*/
+	}
+	:global(h1) {
+		font-family: 'PT Sans', sans-serif;
+		font-weight: bold;
+	}
+
+	
+
+	.container {
+		margin: auto;
+		width: 100%;
+		border-radius: 10px;
+		box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.5);
+		line-height: 18.4px;
+		margin-top: 20px;
+		padding: 20px;
+	}
+	.entrada {
+		padding: 3rem;
+		border-bottom: 1px solid #e1e1e1;
+		margin-bottom: 2rem;
+	}
+</style>
